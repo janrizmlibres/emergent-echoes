e1c307a004a0a7dd5fd96696a256d10ce8bfeb40
Janriz Libres <libres.janriz@gmail.com>
2024-12-31 15:40:57 +0800

    feat: refactor ThiefTrait to enhance resource evaluation logic

--- Utilities/Traits/ThiefTrait.cs
+++ Utilities/Traits/ThiefTrait.cs
@@ -1,34 +1,82 @@
+using EmergentEchoes.addons.NPC2DNode;
+using EmergentEchoes.addons.NPCNode;
+using EmergentEchoes.Entities.Actors;
+using EmergentEchoes.Utilities.World;
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EmergentEchoes.Utilities.Traits
 {
-    public class ThiefTrait : ITrait
+    public class ThiefTrait : Trait
     {
+        private readonly NPC _owner;
+        private readonly List<ResourceStat> _resources;
+        private readonly List<Actor> _actors;
         private readonly float _weight;
 
-        public ThiefTrait(float weight)
+        public ThiefTrait(NPC owner, List<ResourceStat> resources, float weight)
         {
+            _owner = owner;
+            _resources = resources;
             _weight = weight;
+            _actors = _sensor.GetActors();
         }
 
-        public Tuple<string, float> EvaluateAction()
+        public override Tuple<string, float> EvaluateAction()
         {
-            // Calculate score based on:
-            // - Current needs/resources
-            // - Memory of past attempts
-            // - Probability of success
-            // Return weighted score
-            // return score * _weight;
-            return new("Thief", 0);
+            List<ResourceStat> tangibleResources;
+            HashSet<ResourceStat> evaluatedResources = new();
+            ResourceStat selectedResource = null;
+
+            tangibleResources = _resources.Where(resource => resource.IsTangible).ToList();
+
+            while (evaluatedResources.Count != _resources.Count && selectedResource == null)
+            {
+                selectedResource = tangibleResources
+                   .Where(resource => resource.IsImbalanced() && !evaluatedResources.Contains(resource))
+                   .First();
+
+                selectedResource ??= tangibleResources
+                    .Where(resource => !evaluatedResources.Contains(resource))
+                    .First();
+
+                float chosenLikelihood = 0;
+                Actor chosenActor = null;
+
+                List<Actor> otherActors = _actors
+                    .Where(actor => actor.GetInstanceId() != _owner.GetInstanceId()).ToList();
+
+                foreach (Actor actor in otherActors)
+                {
+                    if (actor.HasResource(selectedResource))
+                    {
+                        float likelihood = _owner.Thief * _weight * selectedResource.Value;
+                        if (likelihood > chosenLikelihood)
+                        {
+                            chosenLikelihood = likelihood;
+                            chosenActor = actor;
+                        }
+                    }
+                }
+
+                if (chosenActor != null)
+                {
+                    return new Tuple<string, float>($"Steal {selectedResource.ResourceType} from {chosenActor.Name}", chosenLikelihood);
+                }
+
+                evaluatedResources.Add(selectedResource);
+                selectedResource = null;
+            }
+
+            return new Tuple<string, float>(string.Empty, 0f);
         }
 
-        public bool ShouldActivate()
+        public override bool ShouldActivate(SocialPractice practice)
         {
-            // Check if conditions are right to activate this trait
-            // E.g. are we desperate for resources?
-            // return conditions;
-            return false;
+            return practice.PracticeType == SocialPractice.Practice.Proactive;
         }
+
     }
 }
