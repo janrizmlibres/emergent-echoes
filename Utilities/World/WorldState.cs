e1712c2749b1e7160809cdc553b8f6edfbf0efe5
Janriz Libres <libres.janriz@gmail.com>
2025-01-02 20:04:40 +0800

    feat: enhance WorldState to manage global event records and crime tracking

--- Utilities/World/WorldState.cs
+++ Utilities/World/WorldState.cs
@@ -1,4 +1,6 @@
 using EmergentEchoes.Entities.Actors;
+using EmergentEchoes.Utilities.Components.Records;
+using EmergentEchoes.Utilities.Events;
 using Godot;
 using System;
 using System.Collections.Generic;
@@ -15,7 +17,24 @@ namespace EmergentEchoes
 
 		private readonly List<Actor> _actors = new();
 
-		// TODO: Consider limiting access to actors
+		private readonly List<Node2D> _structures = new();
+
+		// Actions and interactions that have taken place and what actors performed them
+		private readonly List<Event> _globalRecords = new();
+
+		// Which crimes are publicly known and not yet solved
+		private readonly List<Crime> _unsolvedCrimes = new();
+
+		// Solved crimes and who apprehended the criminal
+		private readonly List<Crime> _solvedCrimes = new();
+
+		// What tasks are delegated to which individuals
+		private readonly List<Event> _taskRecords = new();
+
+		// Workplaces and who they belong to (Possibly _structures)
+		// Current date (?)
+
+		// TODO: Consider limiting access to data
 
 		public List<Actor> GetActors()
 		{
@@ -25,11 +44,6 @@ namespace EmergentEchoes
 		public void AddActor(Actor actor)
 		{
 			_actors.Add(actor);
-		}
-
-		public void RemoveActor(Actor actor)
-		{
-			_actors.Remove(actor);
 		}
 	}
 }
