a7cd5640a7073cf07dc0c4760de9415d967f8841
Janriz Libres <libres.janriz@gmail.com>
2025-01-02 20:05:11 +0800

    feat: add LongTermMemory and ShortTermMemory properties to Memorizer class

--- Utilities/Internal/Memorizer.cs
+++ Utilities/Internal/Memorizer.cs
@@ -1,10 +1,14 @@
+using EmergentEchoes.Utilities.Events;
 using Godot;
 using System;
+using System.Collections.Generic;
 
 namespace EmergentEchoes.Utilities.Internal
 {
     public class Memorizer
     {
+        public List<Event> LongTermMemory { get; set; } = new();
 
+        public LinkedList<Event> ShortTermMemory { get; set; } = new();
     }
 }
