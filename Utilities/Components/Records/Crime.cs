c6a27a3b8dfb0466b150dc0799ddf0bc1510c6df
Janriz Libres <libres.janriz@gmail.com>
2025-01-07 03:26:02 +0800

    refactor: remove Crime record to simplify crime management

--- Utilities/Components/Records/Crime.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using EmergentEchoes.addons.NPC2DNode;
-using EmergentEchoes.Utilities.Components.Enums;
-
-namespace EmergentEchoes.Utilities.Components.Records
-{
-    public record Crime(CrimeType Type, NPC2D Criminal, NPC2D Victim, IReadOnlyList<NPC2D> Witnesses);
-}
