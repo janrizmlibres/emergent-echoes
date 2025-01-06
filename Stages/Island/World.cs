d37599cade9f73906eee798c970d60c07c5b2427
Janriz Libres <libres.janriz@gmail.com>
2025-01-07 04:26:03 +0800

    refactor: comment out unused actor initialization logic in World.cs for clarity

--- Stages/Island/World.cs
+++ Stages/Island/World.cs
@@ -1,6 +1,4 @@
-using EmergentEchoes.Entities.Actors;
 using Godot;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,21 +8,21 @@ namespace EmergentEchoes
 	{
 		public override void _Ready()
 		{
-			foreach (Node child in GetChildren())
-			{
-				if (child is Actor actor)
-				{
-					WorldState.Instance.AddActor(actor);
-				}
-			}
+			// foreach (Node child in GetChildren())
+			// {
+			// 	if (child is Actor actor)
+			// 	{
+			// 		WorldState.Instance.AddActor(actor);
+			// 	}
+			// }
 
-			List<Actor> actors = WorldState.Instance.GetActors();
+			// List<Actor> actors = WorldState.Instance.GetActors();
 
-			foreach (Actor actor in actors)
-			{
-				List<Actor> others = actors.Where(a => a != actor).ToList();
-				actor.InitializeInternalState(others);
-			}
+			// foreach (Actor actor in actors)
+			// {
+			// 	List<Actor> others = actors.Where(a => a != actor).ToList();
+			// 	actor.InitializeInternalState(others);
+			// }
 		}
 	}
 }
