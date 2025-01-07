6a24a3d06c4af10fa2fec248f287697e41452e15
Janriz Libres <libres.janriz@gmail.com>
2025-01-07 14:56:20 +0800

    refactor: enhance ActorTag2D with parent node checks and configuration warnings

--- addons/NPCProcGen/Nodes/ActorTag2D/ActorTag2D.cs
+++ addons/NPCProcGen/Nodes/ActorTag2D/ActorTag2D.cs
@@ -1,7 +1,9 @@
 using Godot;
 using NPCProcGen.Core.Components;
 using NPCProcGen.Core.Components.Enums;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NPCProcGen
 {
@@ -13,21 +15,56 @@ namespace NPCProcGen
         [Export(PropertyHint.Range, "1,100,")]
         public int FoodValue { get; set; } = 100;
 
-        protected Node2D _parent;
-
         // TODO: Consider converting resources into dictionary for constant access
         public Dictionary<ResourceType, ResourceStat> Resources { get; private set; } = new();
+
+        protected Node2D _parent;
 
         public override void _Ready()
         {
             if (Engine.IsEditorHint()) return;
 
-            _parent = GetParent() as Node2D;
-
             // TODO: Consider implementing a resource manager
 
             Resources.Add(ResourceType.Money, new ResourceStat(ResourceType.Money, MoneyValue, 1));
             Resources.Add(ResourceType.Food, new ResourceStat(ResourceType.Food, FoodValue, 1));
+        }
+
+        public override void _EnterTree()
+        {
+            if (Engine.IsEditorHint())
+            {
+                CheckParent();
+            }
+        }
+
+        public override string[] _GetConfigurationWarnings()
+        {
+            List<string> warnings = new();
+
+            if (_parent == null)
+            {
+                warnings.Add("The ActorTag2D can be used only under a Node2D inheriting parent node.");
+            }
+
+            return warnings.ToArray();
+        }
+
+        protected void CheckParent()
+        {
+            _parent = GetParent() as Node2D;
+            UpdateConfigurationWarnings();
+
+            if (_parent == null)
+            {
+                SetProcess(false);
+                SetPhysicsProcess(false);
+            }
+            else
+            {
+                SetProcess(true);
+                SetPhysicsProcess(true);
+            }
         }
 
         public bool HasResource(ResourceType type)
