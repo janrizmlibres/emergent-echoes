454253f86006f56bfa7f8a89e7146ffba3387085
Janriz Libres <libres.janriz@gmail.com>
2025-01-08 23:46:45 +0800

    refactor: add FleeState class for NPC fleeing behavior and remove WanderState class

---
+++ addons/NPCProcGen/Core/States/FleeState.cs
@@ -0,0 +1,39 @@
+using Godot;
+using NPCProcGen.Core.Actions;
+
+namespace NPCProcGen.Core.States
+{
+    public class FleeState : ActionState
+    {
+        private static readonly float _fleeDistance = 100;
+        private readonly NPCAction _action;
+
+        public FleeState(NPCAction action, NPCAgent2D owner) : base(owner)
+        {
+            _action = action;
+        }
+
+        public override void Enter()
+        {
+            GD.Print("FleeState Enter");
+
+            Vector2 fleePosition = GetRandomFleePosition(_owner.Parent.GlobalPosition);
+            _action.MoveToTarget(fleePosition);
+            _owner.OnFinishNavigation += CompleteState;
+        }
+
+        private static Vector2 GetRandomFleePosition(Vector2 center)
+        {
+            float angle = GD.Randf() * 2 * Mathf.Pi;
+
+            // Get random radius (using square root for uniform distribution)
+            float random_radius = Mathf.Sqrt(GD.Randf()) * _fleeDistance;
+
+            // Convert polar coordinates to Cartesian
+            return center + new Vector2(
+                Mathf.Cos(angle) * random_radius,
+                Mathf.Sin(angle) * random_radius
+            );
+        }
+    }
+}
