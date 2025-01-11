449901ca6d5ece85b573943ee64e883296954d3a
Janriz Libres <libres.janriz@gmail.com>
2025-01-12 07:36:05 +0800

    refactor: reorganize relationship methods and add GetActorLocation for clarity

--- addons/NPCProcGen/Core/Internal/Memorizer.cs
+++ addons/NPCProcGen/Core/Internal/Memorizer.cs
@@ -13,6 +13,43 @@ namespace NPCProcGen.Core.Internal
 
         private readonly Dictionary<ActorTag2D, ActorData> _actorData = new();
 
+        public void Initialize(List<ActorTag2D> actors)
+        {
+            foreach (ActorTag2D actor in actors)
+            {
+                _actorData.Add(actor, new ActorData(actor));
+            }
+        }
+
+        // ! Remove debug function in production
+        public void PrintActorMemory()
+        {
+            foreach (KeyValuePair<ActorTag2D, ActorData> kvp in _actorData)
+            {
+                GD.Print($"Actor: {kvp.Key.Parent.Name}");
+                GD.Print($"Position: {kvp.Value.LastKnownPosition}");
+                GD.Print($"Relationship: {kvp.Value.Relationship}");
+            }
+        }
+
+        public void ProcessUpdates(double delta)
+        {
+            foreach (ActorData actorData in _actorData.Values)
+            {
+                actorData.Update(delta);
+            }
+        }
+
+        public void UpdateActorLocation(ActorTag2D actor, Vector2 location)
+        {
+            _actorData[actor].LastKnownPosition = location;
+        }
+
+        public Vector2? GetActorLocation(ActorTag2D actor)
+        {
+            return _actorData[actor].LastKnownPosition;
+        }
+
         // * Hostile: -15 to -11
         // * Distrusted: -10 to -6
         // * Unfriendly: -5 to -1
@@ -34,38 +71,6 @@ namespace NPCProcGen.Core.Internal
         public bool IsClose(ActorTag2D actor)
         {
             return _actorData[actor].Relationship > 15;
-        }
-
-        public void Initialize(List<ActorTag2D> actors)
-        {
-            foreach (ActorTag2D actor in actors)
-            {
-                _actorData.Add(actor, new ActorData(actor));
-            }
-        }
-
-        // ! Remove debug function in production
-        public void PrintActorMemory()
-        {
-            foreach (KeyValuePair<ActorTag2D, ActorData> kvp in _actorData)
-            {
-                GD.Print($"Actor: {kvp.Key.Parent.Name}");
-                GD.Print($"Position: {kvp.Value.LastKnownPosition}");
-                GD.Print($"Relationship: {kvp.Value.Relationship}");
-            }
-        }
-
-        public void UpdateActorLocation(ActorTag2D actor, Vector2 location)
-        {
-            _actorData[actor].LastKnownPosition = location;
-        }
-
-        public void ProcessUpdates(double delta)
-        {
-            foreach (ActorData actorData in _actorData.Values)
-            {
-                actorData.Update(delta);
-            }
         }
     }
 }
