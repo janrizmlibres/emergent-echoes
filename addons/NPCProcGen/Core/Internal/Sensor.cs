a03e0b50df2c130755cbf26b5f8b1cbdba6b115d
Janriz Libres <libres.janriz@gmail.com>
2025-01-12 07:36:02 +0800

    refactor: add TODO comment to restrict world state access to Sensor class

--- addons/NPCProcGen/Core/Internal/Sensor.cs
+++ addons/NPCProcGen/Core/Internal/Sensor.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 
 namespace NPCProcGen.Core.Internal
 {
+    // TODO: Make this class the only component that can access the world state
     public class Sensor
     {
         private readonly WorldState _worldState;
