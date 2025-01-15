7064aa39ed730922c549d9048972af0d0d61d02e
Janriz Libres <libres.janriz@gmail.com>
2025-01-15 14:35:49 +0800

    refactor: remove ActorDetected event from NotifManager for simplification

--- addons/NPCProcGen/Core/Helpers/NotifManager.cs
+++ addons/NPCProcGen/Core/Helpers/NotifManager.cs
@@ -6,7 +6,6 @@ namespace NPCProcGen.Core.Helpers
     {
         public event Action NavigationComplete;
         public event Action TheftComplete;
-        public event Action<ActorTag2D> ActorDetected;
 
         public void NotifyNavigationComplete()
         {
@@ -16,11 +15,6 @@ namespace NPCProcGen.Core.Helpers
         public void NotifyTheftComplete()
         {
             TheftComplete?.Invoke();
-        }
-
-        public void NotifyActorDetected(ActorTag2D actor)
-        {
-            ActorDetected?.Invoke(actor);
         }
     }
 }
