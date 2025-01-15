6cef991b127edae14eb201d80746d31464a4c055
Janriz Libres <libres.janriz@gmail.com>
2025-01-15 14:35:21 +0800

    refactor: rename Value property to Amount and update related logic in ResourceStat

--- addons/NPCProcGen/Core/Components/ResourceStat.cs
+++ addons/NPCProcGen/Core/Components/ResourceStat.cs
@@ -1,34 +1,38 @@
 using NPCProcGen.Core.Components.Enums;
-using System.Collections.Generic;
+using System;
 
 namespace NPCProcGen.Core.Components
 {
     public class ResourceStat
     {
-        public static readonly List<ResourceType> TangibleTypes = new()
-        {
-            ResourceType.Money,
-            ResourceType.Food
-        };
-
         public ResourceType Type { get; private set; }
 
-        public float Value { get; private set; }
+        public float Amount
+        {
+            get => amount;
+            set
+            {
+                amount = Math.Clamp(value, 0, Type == ResourceType.Money ? 1000000 : 100);
+                amount = (float)(Type == ResourceType.Money ? Math.Floor(amount) : amount);
+            }
+        }
+
         public float Weight { get; private set; }
 
         public float LowerThreshold { get; private set; }
         public float UpperThreshold { get; private set; }
 
+        private float amount;
+
         public ResourceStat(ResourceType type, float value, float weight)
         {
             Type = type;
-            Value = value;
+            Amount = value;
             Weight = weight;
 
             // TODO: Implement thresholds for each resource type
-
-            LowerThreshold = 0;
-            UpperThreshold = 100;
+            LowerThreshold = 30;
+            UpperThreshold = 80;
         }
 
         public bool IsImbalanced()
@@ -38,12 +42,12 @@ namespace NPCProcGen.Core.Components
 
         public bool IsDeficient()
         {
-            return Value < LowerThreshold;
+            return amount < LowerThreshold;
         }
 
         public bool IsSaturated()
         {
-            return Value > UpperThreshold;
+            return amount > UpperThreshold;
         }
     }
 }
