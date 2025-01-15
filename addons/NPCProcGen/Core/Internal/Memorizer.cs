a58d57123613bdd69ac8736b9280abfe0e902be5
Janriz Libres <libres.janriz@gmail.com>
2025-01-15 19:34:42 +0800

    fix: add TODO comment to address existing bug in UpdateActorLocation method

--- addons/NPCProcGen/Core/Internal/Memorizer.cs
+++ addons/NPCProcGen/Core/Internal/Memorizer.cs
@@ -30,6 +30,7 @@ namespace NPCProcGen.Core.Internal
             }
         }
 
+        // TODO: Fix existing bug
         public void UpdateActorLocation(ActorTag2D actor, Vector2 location)
         {
             DebugTool.Assert(_actorData.ContainsKey(actor), $"Actor {actor.Parent.Name} not found in memorizer.");
