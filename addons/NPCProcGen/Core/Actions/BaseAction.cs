25bf6513f2b0a5aed56121356e035bfb96c5f15c
Janriz Libres <libres.janriz@gmail.com>
2025-01-28 07:12:37 +0800

    feat: add AssignActor method to link NPCAgent2D with TalkState

--- addons/NPCProcGen/Core/Actions/BaseAction.cs
+++ addons/NPCProcGen/Core/Actions/BaseAction.cs
@@ -74,6 +74,14 @@ namespace NPCProcGen.Core.Actions
             return _currentState is INavigationState state && state.IsNavigating();
         }
 
+        public void AssignActor(NPCAgent2D actor)
+        {
+            if (_currentState is TalkState talkState)
+            {
+                talkState.Partner = actor;
+            }
+        }
+
         /// <summary>
         /// Updates the action.
         /// </summary>
