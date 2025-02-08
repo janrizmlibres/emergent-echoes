111c8ad7c6ad77776504330f5ec108d77eb0cf6c
Clint-Galendez-20210021497 <20210021497@my.xu.edu.ph>
2025-02-08 08:10:47 +0800

    feat: enhance Garreth's behavior with new collision area and update Silas' actor detection

--- Entities/Actors/NPCs/Silas/beehave/chase_actor.gd
+++ Entities/Actors/NPCs/Silas/beehave/chase_actor.gd
@@ -4,14 +4,14 @@ signal move_player(patrol_location: Vector2)
 
 @onready var blackboard_object = $"../../../../Blackboard"
 
-var player_near: bool = false
+var actor_near: bool = false
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	if blackboard.get_value("current_state") != "chasing":
 		return FAILURE
 	
-	if player_near:
-		player_near = false
+	if actor_near:
+		actor_near = false
 		blackboard.set_value("current_state", "stealing")
 		return SUCCESS
 	
@@ -19,6 +19,9 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 	return RUNNING 
 
 func _on_steal_area_body_entered(body: Node2D) -> void:
-	if body.get_node_or_null("ActorTag2D"):
-		player_near = true
+	if body.get_node_or_null("ActorTag2D") == null:
+		return
+	
+	if body.get_name() == blackboard_object.get_value("actor").get_name():
+		actor_near = true
 	pass # Replace with function body.
