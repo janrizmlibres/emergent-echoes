bccdb4cf19a2efaf46f91b6ce4da383fa4494f65
Janriz Libres <libres.janriz@gmail.com>
2025-02-21 21:24:39 +0800

    Add ActorContext class to manage actor-related components and signal emission

---
+++ addons/NPCProcGen/Core/Internal/ActorContext.cs
@@ -0,0 +1,37 @@
+using Godot;
+using NPCProcGen.Core.Helpers;
+
+namespace NPCProcGen.Core.Internal
+{
+    public class ActorContext
+    {
+        public Sensor Sensor { get; set; }
+        public Memorizer Memorizer { get; set; }
+        public Executor Executor { get; set; }
+
+        public ActorTag2D Actor { get; private set; }
+        public Node2D ActorNode2D { get; private set; }
+
+        public ActorContext(ActorTag2D actor)
+        {
+            Actor = actor;
+            ActorNode2D = actor.GetParent<Node2D>();
+        }
+
+        public NPCAgent2D GetNPCAgent2D()
+        {
+            return Actor as NPCAgent2D;
+        }
+
+        public NPCMemorizer GetNPCMemorizer()
+        {
+            return Memorizer as NPCMemorizer;
+        }
+
+        public void EmitSignal(StringName signalName, params Variant[] args)
+        {
+            Error result = Actor.EmitSignal(signalName, args);
+            DebugTool.Assert(result != Error.Unavailable, "Signal emitted error");
+        }
+    }
+}
