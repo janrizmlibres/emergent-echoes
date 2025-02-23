8837760063658f0400bee6dba86bccb34f79d24a
Janriz Libres <libres.janriz@gmail.com>
2025-02-23 19:32:28 +0800

    Refactor AssessAction to set StartingState directly in InitializeStates for improved state management

--- addons/NPCProcGen/Core/Actions/AssessAction.cs
+++ addons/NPCProcGen/Core/Actions/AssessAction.cs
@@ -6,15 +6,11 @@ namespace NPCProcGen.Core.Actions
 {
     public class AssessAction : BaseAction
     {
-        private AssessState _reviewState;
-
         public AssessAction(ActorContext context) : base(context, ActionType.Assess) { }
 
         protected override void InitializeStates()
         {
-            _reviewState = new AssessState(_actorContext, _stateContext);
+            _stateContext.StartingState = new AssessState(_actorContext, _stateContext);
         }
-
-        protected override BaseState GetStartingState() => _reviewState;
     }
 }
