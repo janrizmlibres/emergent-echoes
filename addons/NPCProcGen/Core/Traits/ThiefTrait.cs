0d78dc507dd5954e3cca9f9acbd000487f6eb38d
Janriz Libres <libres.janriz@gmail.com>
2025-03-02 13:19:20 +0800

    Refactor trait action evaluation methods for improved clarity and efficiency

--- addons/NPCProcGen/Core/Traits/ThiefTrait.cs
+++ addons/NPCProcGen/Core/Traits/ThiefTrait.cs
@@ -8,57 +8,22 @@ using NPCProcGen.Core.Internal;
 
 namespace NPCProcGen.Core.Traits
 {
-    /// <summary>
-    /// Represents a trait for stealing resources.
-    /// </summary>
     public class ThiefTrait : Trait
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ThiefTrait"/> class.
-        /// </summary>
-        /// <param name="owner">The owner of the trait.</param>
-        /// <param name="weight">The weight of the trait.</param>
-        /// <param name="sensor">The sensor associated with the trait.</param>
-        /// <param name="memorizer">The memorizer associated with the trait.</param>
         public ThiefTrait(ActorContext context, float weight) : base(context, weight) { }
 
-        /// <summary>
-        /// Evaluates an action based on the given social practice.
-        /// </summary>
-        /// <param name="practice">The social practice to evaluate.</param>
-        /// <returns>A tuple containing the evaluated action and its weight.</returns>
-        public override Tuple<BaseAction, float> EvaluateAction(SocialPractice practice)
-        {
-            if (practice == SocialPractice.Proactive)
-            {
-                return EvaluateProactiveAction();
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        /// Evaluates a proactive action for stealing resources.
-        /// </summary>
-        /// <returns>A tuple containing the evaluated action and its weight.</returns>
-        private Tuple<BaseAction, float> EvaluateProactiveAction()
+        protected override void EvaluateProactiveAction()
         {
             ResourceManager resourceMgr = ResourceManager.Instance;
-            List<Tuple<BaseAction, float>> actionCandidates = new();
 
             foreach (ResourceType type in resourceMgr.TangibleTypes)
             {
-                if (resourceMgr.IsDeficient(_actorCtx.Actor, type))
-                {
-                    EvaluateInteraction(
-                        actionCandidates, type,
-                        peerActors => PickActor(peerActors, type),
-                        ActionType.Theft
-                    );
-                }
+                EvaluateInteraction(
+                    type,
+                    peerActors => PickActor(peerActors, type),
+                    ActionType.Theft
+                );
             }
-
-            return actionCandidates.OrderByDescending(tuple => tuple.Item2).FirstOrDefault();
         }
 
         // ! Remove in production
@@ -67,7 +32,7 @@ namespace NPCProcGen.Core.Traits
             List<Tuple<BaseAction, float>> actionCandidates = new();
 
             EvaluateInteraction(
-                actionCandidates, resType,
+                resType,
                 peerActors => PickActor(peerActors, resType),
                 ActionType.Theft
             );
@@ -80,7 +45,7 @@ namespace NPCProcGen.Core.Traits
             foreach (ActorTag2D actor in peerActors)
             {
                 if (!_actorCtx.Memorizer.IsTrusted(actor)
-                    || !ResourceManager.Instance.IsDeficient(actor, type))
+                    || !ResourceManager.Instance.IsDeficient(type, actor))
                 {
                     return actor;
                 }
