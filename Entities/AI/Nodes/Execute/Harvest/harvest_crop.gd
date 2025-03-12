e0baca67362232c9d8332ea4b6950246935022e3
Janriz Libres <libres.janriz@gmail.com>
2025-03-12 21:02:35 +0800

    folder restructure

--- Entities/AI/Nodes/Execute/Harvest/harvest_crop.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends ActionLeaf
-
-func tick(_actor: Node, blackboard: Blackboard) -> int:
-	var crop_tile = blackboard.get_value("crop_tile")
-	crop_tile.status = CropTile.Status.DORMANT
-	crop_tile.is_attended = false
-	crop_tile.sprite.visible = false
-	return SUCCESS
